9e2e18127fed55cee45f91818324aac3a2e6444a
cool-akash <akj14062000.com>
2022-02-02 19:53:55 +0530

    Before Login

--- backend/server.js
+++ backend/server.js
@@ -1,22 +1,31 @@
 const express = require("express");
-const notes = require("./data/notes")
+const notes = require("./data/notes");
 const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+const userRoutes = require("./routes/userRoutes");
+const { notFound, errorHandler } = require("./middlewares/errorMiddleware");
+
 const app = express();
 dotenv.config();
+connectDB();
+app.use(express.json());
 
 app.get("/", (req, res) => {
-   res.send("API is running...")
+    res.send("API is Running...");
 });
 
 app.get("/api/notes", (req, res) => {
-   res.json(notes);
+    res.json(notes);
 });
 
-app.get("/api/notes/:id", (req, res) => {
-   const note = notes.find((n) => n._id === req.params.id);
-   res.send(note);
-})
+
+
+app.use("/api/users", userRoutes);
+
+app.use(notFound);
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, console.log(`Server started at PORT ${PORT}`));
+app.listen(5000, console.log(`Server started on PORT ${PORT}`));
+
